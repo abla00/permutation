3ea606ad9859d8e9400c91077decaaf1a8c6bd8c
abla <abla00@gmail.com>
2018-10-12 09:11:01 +0000

    Display total result amount

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,25 @@
 class WelcomeController < ApplicationController
   def view
-    @q = params[:q]
+    @output = output(params[:q])
   end
+  
+  private
+    def output(string)
+      chars = string.split('')
+      
+      permute([], chars, "", chars.length)
+    end
+    
+    def permute(array, chars, prefix, max)
+      if (max == 0)
+        array.push prefix
+      else
+        length = chars.length - 1
+        for i in (0..length)
+          permute(array, chars, prefix + chars[i], max - 1)
+        end
+      end
+      
+      array
+    end
 end
